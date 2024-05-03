baf6438eb204b7efd1b46493dbe82f55eb006898
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-05-03 17:17:26 +0530

    complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -27,6 +27,8 @@ export default function Profile() {
     const [fileUploadError, setFileUploadError] = useState(false);
     const [formData, setFormData] = useState({});
     const [updateSuccess, setUpdateSuccess] = useState(false);
+    const [showListingsError, setShowListingsError] = useState(false);
+    const [userListings, setUserListings] = useState([]);
     const dispatch = useDispatch();
 
     // Firebase Storage
@@ -68,6 +70,7 @@ export default function Profile() {
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value });
     };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -109,9 +112,8 @@ export default function Profile() {
     };
 
     const handleSignOut = async () => {
-
         try {
-            dispatch(signOutUserStart())
+            dispatch(signOutUserStart());
             const res = await fetch('/api/auth/signout');
             const data = await res.json();
             if (data.success === false) {
@@ -122,7 +124,22 @@ export default function Profile() {
         } catch (error) {
             dispatch(deleteUserFailure(data.message));
         }
-    }
+    };
+
+    const handleShowListings = async () => {
+        try {
+            setShowListingsError(false);
+            const res = await fetch(`/api/user/listings/${currentUser._id}`);
+            const data = await res.json();
+            if (data.success === false) {
+                setShowListingsError(true);
+                return;
+            }
+            setUserListings(data);
+        } catch (error) {
+            setShowListingsError(true);
+        }
+    };
 
     return (
         <div className='p-3 max-w-lg mx-auto'>
@@ -182,7 +199,9 @@ export default function Profile() {
                     className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
                     {loading ? 'Loading...' : 'Update'}
                 </button>
-                <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+                <Link
+                    className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+                    to={'/create-listing'}>
                     Create Listing
                 </Link>
             </form>
@@ -192,12 +211,48 @@ export default function Profile() {
                     className='text-red-700 cursor-pointer'>
                     Delete account
                 </span>
-                <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
+                <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
+                    Sign out
+                </span>
             </div>
             <p className='text-red-700 mt-5'>{error ? error : ''}</p>
             <p className='text-green-700 mt-5'>
                 {updateSuccess ? 'User updated successfully!' : ''}
             </p>
+            <button onClick={handleShowListings} className='text-green-700 w-full'>
+                Show Listings
+            </button>
+            <p className='text-red-700 mt-5'>
+                {showListingsError ? 'Error showing listings' : ''}
+            </p>
+            {userListings &&
+                userListings.length > 0 &&
+                <div className="flex flex-col gap-4">
+                    <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+                    {userListings.map((listing) => (
+                        <div
+                            key={listing._id}
+                            className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+                            <Link to={`/listing/${listing._id}`}>
+                                <img
+                                    src={listing.imageUrls[0]}
+                                    alt='listing cover'
+                                    className='h-16 w-16 object-contain'
+                                />
+                            </Link>
+                            <Link
+                                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                                to={`/listing/${listing._id}`}>
+                                <p>{listing.name}</p>
+                            </Link>
+                            <div className='flex flex-col item-center'>
+                                <button className='text-red-700 uppercase'>Delete</button>
+                                <button className='text-green-700 uppercase'>Edit</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            }
         </div>
     );
 }
