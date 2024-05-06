1c2d363ea986a9ecccc43725838aa7ed362bc9ba
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-05-06 11:51:46 +0530

    add logo in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { FaSearch } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+import Logo from "../assets/Logo.png";
 
 export default function Header() {
     const { currentUser } = useSelector((state) => state.user);
@@ -28,8 +29,11 @@ export default function Header() {
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
                 <Link to='/'>
                     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                        <span className='text-slate-500'>Estate</span>
-                        <span className='text-slate-700'>Vista</span>
+                        <img src={Logo} alt="logo" width={60} height={60} className='rounded-full' />
+                        <div className='mt-4 ml-2'>
+                            <span className='text-slate-500'>Estate</span>
+                            <span className='text-slate-700'>Vista</span>
+                        </div>
                     </h1>
                 </Link>
                 <form
