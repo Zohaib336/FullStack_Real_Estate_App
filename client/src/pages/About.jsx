cd4b04cecc4854e3ce020358bfb2d25e7cfe4692
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-05-06 11:37:57 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,12 @@
 export default function About() {
     return (
-        <div>About</div>
+        <div className='py-20 px-4 max-w-6xl mx-auto'>
+            <h1 className='text-3xl font-bold mb-4 text-slate-800'>About EstateVista</h1>
+            <p className='mb-4 text-slate-700'>EstateVista is a top real estate agency. We focus on assisting clients with purchasing, selling, and renting properties in sought-after areas. Our skilled agents are committed to delivering outstanding service, ensuring a hassle-free experience for buyers and sellers.</p>
+            <p className='mb-4 text-slate-700'>
+                We're here to assist you in reaching your real estate objectives with tailored guidance and local market knowledge. Whether you're buying, selling, or renting a property, we've got you covered at every stage. Our goal is to provide expert support and personalized service to ensure your real estate journey is smooth and successful.
+            </p>
+            <p className='mb-4 text-slate-700'>Our team knows a lot about real estate and we really care about helping people. We want buying or selling a house to feel great for everyone involved. We work hard to give our clients the best possible experience.</p>
+        </div>
     )
 }
